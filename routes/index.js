e03c70385353a9bad0ca51c34bd34cc3eb4e64d3
Ricardo Rauch <ricardo@gravityonmars.com>
2012-08-12 04:17:27 -0300

    fixes

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+
+exports.admin_index = function(req, res){
+  res.render('admin/index', { title: 'Express', layout: 'admin/layout'})
+};
+
+exports.admin_edit = function(req, res){
+  res.render('admin/edit', { title: 'Express', layout: 'admin/layout'})
+};

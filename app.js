77a7f7aae3c57a3a2408737cabed27ff90faaf41
Ricardo Rauch <ricardo@gravityonmars.com>
2012-08-12 04:31:40 -0300

    fixes

--- app.js
+++ app.js
@@ -30,11 +30,34 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate(..., function (err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
 // Routes
 
 app.get('/', routes.index);
 app.get('/admin', routes.admin_index);
 app.get('/admin/edit', routes.admin_edit);
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
+
+
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
